81fc3ec02ffa3ca4a6320d13a71c62bae7d48729
murad <murad@192.168.1.136>
2020-02-28 21:59:57 +0200

    modify

--- src/soft/Calculater.java
+++ src/soft/Calculater.java
@@ -5,19 +5,8 @@ public class Calculater {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This Calculater can just add two numbers.\n");
-		System.out.println("Enter the first Number:");
-		
-		Scanner scan= new Scanner (System.in);
-		int x = scan.nextInt();
-		
-		System.out.println("Enter the second Number:");
-		
-		int y= scan.nextInt();
-		
-		int z= x + y;
-		
-		System.out.println("\nSummation= " +z);
+		System.out.println("First soft proj");
+
 	}
 
 }
